286bd4defba1d543976d7ea825f27ae1e3399b5d
ypc <627162700@qq.com>
2023-02-28 11:47:02 +0800

    配置端口

--- config/config.ts
+++ config/config.ts
@@ -6,11 +6,16 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
+  history: { type: 'hash' },
+  hash: true,
+  devServer: {
+    port: 8000,
+    host: '0.0.0.0',
+  },
   antd: {
     // 配置开启antd组件库的使用
     dark: false,
   },
-  base: '/',
   layout: {
     // 开启项目视图骨架配置
     name: '周报管理系统',
